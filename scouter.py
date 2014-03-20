808141159d650add496bb9926119eff9f617482a
Alex <alex@renda.org>
2014-03-19 17:02:15 -0700

    made all save to one worksheet, and added all the relevant columns

--- scouter.py
+++ scouter.py
@@ -18,11 +18,36 @@ def get_teams():
     return json.loads(response.read())
 
 workbook = xlwt.Workbook()
-for team in get_teams():
-    sheet = workbook.add_sheet(str(team[u'team_number']))
-    for (i, item) in enumerate(team):
-        sheet.write(0, i, item)
-        sheet.write(1, i, team[item])
+
+sheet = workbook.add_sheet(year + event_code)
+i = 0
+sheet.write(0, i, "Number")
+i += 1
+sheet.write(0, i, "Nickname")
+i += 1
+sheet.write(0, i, "1st Regional")
+i += 1
+sheet.write(0, i, "Record")
+i += 1
+sheet.write(0, i, "OPR")
+i += 1
+sheet.write(0, i, "Auto Points")
+i += 1
+sheet.write(0, i, "Assist Points")
+i += 1
+sheet.write(0, i, "T&C Points")
+i += 1
+sheet.write(0, i, "Teleop Points")
+i += 1
+sheet.write(0, i, "DPR")
+i += 1
+sheet.write(0, i, "Going to Champs")
+i += 1
+sheet.write(0, i, "Awards")
+
+for (index, team) in enumerate(get_teams()):
+    sheet.write(index+1, 0, team[u'team_number'])
+    sheet.write(index+1, 1, team[u'nickname'])
 
 options = {}
 options['defaultextension'] = '.xls'
@@ -30,7 +55,6 @@ options['filetypes'] = [('all files', '.*'), ('Excel spreadsheets', '.xls')]
 options['initialdir'] = expanduser("~")
 options['initialfile'] = year + event_code + ".xls"
 options['title'] = 'Save as'
-
 try:
     workbook.save(tkFileDialog.asksaveasfilename( **options))
 except:
