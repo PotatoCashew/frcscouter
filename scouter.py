b0ee317ff1054c8163155391da4ed0b8fbcd0936
Alex <alex@renda.org>
2014-03-19 20:20:44 -0700

    added web folder

--- scouter.py
+++ scouter.py
@@ -1,7 +1,9 @@
-import urllib2, xlwt, simplejson as json, tkFileDialog
+import urllib2, xlwt, simplejson as json, tkFileDialog, urllib
 from os.path import expanduser
+from operator import itemgetter
 
-url = "http://www.thebluealliance.com/api/v2/event/"
+url = "http://www.thebluealliance.com/api/v2/"
+first_alliance_url = "http://www.thefirstalliance.org/api/api.json.php?action="
 HEADER_NAME = "x-tba-app-id"
 header649 = "frc649:scouter:1"
 
@@ -9,45 +11,89 @@ print("year:")
 year = raw_input()
 print("event code (e.g. cama)")
 event_code = raw_input()
-url += year + event_code + '/'
 
-def get_teams():
-    req = urllib2.Request(url + "teams")
+def send_request(url):
+    req = urllib2.Request(url)
     req.add_header(HEADER_NAME, header649)
     response = urllib2.urlopen(req)
     return json.loads(response.read())
+	
+def get_teams():
+    return send_request(url + "event/" + year + event_code + '/' + "teams")
 
+def get_detailed_team_info(team_number):
+    return send_request(url + "team/frc" + str(team_number) + "/" + year)
+	
+def write_headers(num_regionals):	
+	i = 0
+	sheet.write(0, i, "Number")
+	i += 1
+	sheet.write(0, i, "Nickname")
+	i += 1
+	for x in range(num_regionals):
+		sheet.write(0, i, "Regional " + str(x+1))
+		i += 1
+		sheet.write(0, i, "Record")
+		i += 1
+		sheet.write(0, i, "power_rating")
+		i += 1
+		sheet.write(0, i, "Auto Points")
+		i += 1
+		sheet.write(0, i, "Assist Points")
+		i += 1
+		sheet.write(0, i, "T&C Points")
+		i += 1
+		sheet.write(0, i, "Teleop Points")
+		i += 1
+		sheet.write(0, i, "DPR")
+		i += 1
+	sheet.write(0, i, "Going to Champs")
+	i += 1
+	sheet.write(0, i, "Awards")
+	
+def get_regional_data(event_code):
+	return None
+	
+def get_power_rating(team, regional, type):
+    url = "http://www.adambots.com/scouting/automated2014/?comp="
+    #parameters = {"team-number" : str(team), "event-code" : regional}
+    #data = urllib.urlencode(parameters)
+    #req = urllib2.Request(url + "team-event-" + type.lower(), data)
+    req = urllib2.Request(url + regional)
+    response1 = urllib2.urlopen(req)
+    print(response1.read())
+    #power_rating_data = json.loads(response1.read())
+    index = 0
+    power_rating = None
+    while (power_rating is None or power_rating is not "null") and index < len(power_rating_data[u'data']):
+		power_rating = power_rating_data[u'data'][index]['OPR' if type.lower() == 'opr' else 'dPR']
+		index+= 1
+	
+    return (power_rating)
+	
 workbook = xlwt.Workbook()
 
 sheet = workbook.add_sheet(year + event_code)
-i = 0
-sheet.write(0, i, "Number")
-i += 1
-sheet.write(0, i, "Nickname")
-i += 1
-sheet.write(0, i, "1st Regional")
-i += 1
-sheet.write(0, i, "Record")
-i += 1
-sheet.write(0, i, "OPR")
-i += 1
-sheet.write(0, i, "Auto Points")
-i += 1
-sheet.write(0, i, "Assist Points")
-i += 1
-sheet.write(0, i, "T&C Points")
-i += 1
-sheet.write(0, i, "Teleop Points")
-i += 1
-sheet.write(0, i, "DPR")
-i += 1
-sheet.write(0, i, "Going to Champs")
-i += 1
-sheet.write(0, i, "Awards")
+max_regionals = 0
+teams = sorted(get_teams(), key=itemgetter('team_number'))
+regional_data = {}
+for (index, team) in enumerate(teams):
+	team_number = team[u'team_number']
+	team_nickname = team[u'nickname']
+	sheet.write(index+1, 0, team_number)
+	sheet.write(index+1, 1, team_nickname)
+	team_info = get_detailed_team_info(team_number)
+	print("Getting data on " + str(team_number) + ": " + team_nickname + " (" + str(index + 1) + "/" + str(len(teams)) + ")")
+	max_regionals = len(team_info[u'events'])-1
+	for regional in team_info[u'events']:
+		print(regional)
+		team_event_code = regional[u'event_code']
+		if not team_event_code == event_code:
+			print(get_power_rating(team_number, team_event_code, 'opr'), get_power_rating(team_number, team_event_code, 'dpr'))
+		#if not regional[u'event_code'] in regional_data:
+			#regional_data[regional[u'event_code']] = 
 
-for (index, team) in enumerate(get_teams()):
-    sheet.write(index+1, 0, team[u'team_number'])
-    sheet.write(index+1, 1, team[u'nickname'])
+write_headers(max_regionals)
 
 options = {}
 options['defaultextension'] = '.xls'
---
+++ web/page.js
@@ -0,0 +1,145 @@
+/*
+This file is part of the Adambots Automated Scouting Kit (AASK).
+
+AASK is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+AASK is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+AASK.  If not, see <http://www.gnu.org/licenses/>.
+
+AASK was started during the 2013 FIRST season by Ben Bray and Curtis Fenner of
+Team 245, the Adambots, for use by other FRC teams.
+*/
+
+//// TABLE CONSTRUCTOR --------------------------------------------------------
+
+/**
+ * A "Table" occurs on a page when consecutive rows of data contains numbers. The row immediately before the data-rows is used as a heading.
+ * 
+ * heading/headings/header/headers :: String[] The names of the columns (in caps) of the data.
+ * data :: Int[row][col]  The data filling the cells. NaNs in most cases where there are no parseable numbers.
+ * raw :: String[row][col] the text filling the cells. data[row][col] == parseFloat(raw[row][col]), essentially.
+ */
+function Table(heading,raw,data) {
+	this.heading = this.headings = this.header = this.headers = heading;
+	this.raw = raw;
+	this.data = data;
+}
+
+//// WEBSITE SCRAPER ----------------------------------------------------------
+
+/**
+ * Cross platform method to get XMLHttpRequest objects. Taken from an article
+ * published by Brett McLaughlin.
+ */
+function createRequest() {
+	var request;
+	try{
+		request = new XMLHttpRequest();
+	} catch(trymicrosoft) {
+		try{
+			request = new ActiveXObject("Msxml2.XMLHTTP");
+		}catch(othermicrosoft){
+			try{
+				request = new ActiveXObject("Microsoft.XMLHTTP");
+			} catch(failed) {
+				request = false;
+			}
+		}
+	}
+	// Ensure Request Existence
+	if(!request) {
+		alert("Unfortunately, your browser cannot utilize AASK. Please enable, or switch to a browser with, XMLHttpRequests! (Chrome, Opera, Safari, IE, others)");
+	}
+	return request;
+}
+
+//// PAGE CONSTRCUTOR ---------------------------------------------------------
+
+/**
+ * Constructors a "page" object with .tables representing the list of "data tables" on the page.
+ *
+ * url: The url after the domain to parse, e.g. "2013comp/Events/MIGBL/rankings.html"
+ *
+ * obj.tables :: Table[] of data tables appearing on page.
+ * obj.ready :: bool True when the data has been parsed and placed into the tables array.
+ */
+function Page(url) {
+	var me = this;
+	this.returnto = function() {
+		if (me.r.readyState != 4) {
+			return;
+		}
+		var text = me.r.responseText.toUpperCase();
+		/*var rows = text.split("<TR"); //I do NOT want the first one (prior to the start of a <TR>:
+		rows.splice(0,1); //Remove the first one.
+		for (var i = 0; i < rows.length; i++) {
+			rows[i] = rows[i].substring(0, rows[i].indexOf("</TR") ); //Stop when the row ends. Requires a </TR> pairing to each <TR>.
+			//Not sure whether or not valid HTML requires it. May be a poor requirement, but fairly predictable and reliable.
+			rows[i] = rows[i].substring(rows[i].indexOf(">") + 1,rows[i].length); //I don't want anything between "<TR" and ">".
+			rows[i] = " " + rows[i].trim() + " ";
+		}
+		var raw = []; // raw[row][col] == string content of cell
+		var data = []; //data[row][col] == int content (or NaN). bool data[row].hasdata means that there are numbers in this row (not just text / headings &c)
+		for (var i = 0; i < rows.length; i++) {
+			//Split up the TD's...
+			raw[i] = [];
+			data[i] = [];
+			var cols = rows[i].split("<TD");
+			cols.splice(0,1);
+			for (var u = 0; u < cols.length; u++) {
+				cols[u] = " <TD" + cols[u]; //Reattach for the purpose of the 'outoftag'
+				cols[u] = outoftag(cols[u].substring(0,cols[u].indexOf("</TD"))); //get content not inside tags.
+				raw[i][u] = cols[u].trim(); //Fill the body! 'raw' refers to EVERY ROW COL pair.
+				data[i][u] = parseFloat(raw[i][u]);
+				var isnumber = raw[i][u].replace(/[0123456789\.]/g,"").trim() == ""; //contains only the decimal, spaces, and numbers.
+				if (isnumber && !isNaN(data[i][u])) {
+					data[i].hasdata = true;
+				}
+			}
+		}*/
+		var raw = [];
+		var data = [];
+		var raw = text.split("\n");
+		for (var i = 0; i < raw.length; i++) {
+			raw[i] = raw[i].split(";");
+			data[i] = [];
+			for (var j = 0; j < raw[i].length; j++) {
+				data[i][j] = parseFloat(raw[i][j]);
+				var isnumber = raw[i][j].replace(/[0123456789\.]/g,"").trim() == ""; //contains only the decimal, spaces, and numbers.
+				if (isnumber && !isNaN(data[i][j])) {
+					data[i].hasdata = true;
+				}
+			}
+		}
+
+		me.tables = [];
+		for (var i = 0; i < data.length; i++) {
+			if (data[i].hasdata) {
+			//New table!!!!!!!!!!!!
+				var ra = [];
+				var dat = [];
+				for (var u = i; u < data.length; u++) {
+					if (!data[u].hasdata) {
+						me.tables[me.tables.length] = new Table(raw[i-1] , ra,dat );
+						i = u;
+						break;
+					} else {
+						ra[ra.length] = raw[u];
+						dat[dat.length] = data[u];
+					}
+				}
+			}
+		}
+		me.ready = true;
+	}
+	this.r = createRequest();
+	this.r.open("GET","?grab=" + url,true);
+	this.r.onreadystatechange = this.returnto;
+	this.r.send();
+}
